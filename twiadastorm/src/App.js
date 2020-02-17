214b996a9863115c7faad309263baa083ecebdc0
Adrian <adrianjmaciasg@gmail.com>
2020-02-17 13:37:14 -0700

    connected/imported bootrap modules added basic search UI

--- twiadastorm/src/App.js
+++ twiadastorm/src/App.js
@@ -1,44 +1,14 @@
 import React, {Component, useState} from 'react';
+import  'bootstrap/dist/css/bootstrap.min.css'
 import logo from './logo.svg';
 import './App.css';
-
-const [hidden, setHidden] = useState(true);
-
-
+import Container from "react-bootstrap/Container";
+import {Searchbar} from "./Components/Searchbar";
 
 function App() {
-
-  return (
-    <div className="App">
-      <title>Twitter Sentiment analysis </title>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Welcome  text= " ---- "/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
-          <p>This should show/hide{toggle}</p>
-          <button onClick={toggle}>Show / Hide </button>
-      </header>
-    </div>
-  );
-
+    return(
+       <Searchbar/>
+    );
 }
-class Welcome extends Component {
-    render() {
-        const { text } = this.props;
-        return(
-            <h1 className="App-title">{text}</h1>
-        )
-    }
-}
+
 export default App;
